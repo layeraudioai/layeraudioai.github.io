6accf55b09811e1e1256fa70b2bbb5baa1e01bd4
brendan <brendancarellmusic@gmail.com>
2026-02-04 00:35:48 -0500

    surround sound fix

--- server.py
+++ server.py
@@ -6,8 +6,31 @@ import os
 PORT = 3000
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-Handler = http.server.SimpleHTTPRequestHandler
+class RedirectHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Redirect to HTTPS for layai.ca domain
+        host = self.headers.get('Host', '')
+        if 'layai.ca' in host:
+            self.send_response(301)
+            self.send_header('Location', f'https://{host}{self.path}')
+            self.end_headers()
+            return
+        # Otherwise serve normally
+        super().do_GET()
+    
+    def do_HEAD(self):
+        # Redirect to HTTPS for layai.ca domain
+        host = self.headers.get('Host', '')
+        if 'layai.ca' in host:
+            self.send_response(301)
+            self.send_header('Location', f'https://{host}{self.path}')
+            self.end_headers()
+            return
+        super().do_HEAD()
+
+Handler = RedirectHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"LayAI server running at http://localhost:{PORT}")
+    print(f"HTTP requests to layai.ca will redirect to HTTPS")
     httpd.serve_forever()
